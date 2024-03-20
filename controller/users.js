6776bfaa7113e87a1e0559dcfb864ba501a6740b
Aylin <aylin_santacruz@hotmail.com>
2024-03-20 15:38:21 -0500

    feat(users.js):Add postUser function to manage users and importing from bcrypt for password encryption

--- controller/users.js
+++ controller/users.js
@@ -1,22 +1,22 @@
 const { connect } = require("../connect");
-const { ObjectId } = require("mongodb");
 const bcrypt = require("bcrypt");
 module.exports = {
-  getUsers: async(req, resp, next) => {
+  getUsers: async (req, resp, next) => {
     const db = await connect();
-  const collection = db.collection("users");
-  const options = {
-    projection: { _id: 1, email:1, role:1 },
-  };
-  const result = await collection.find({},options).toArray();
-  resp.json(result);
+    const collection = db.collection("users");
+    const options = {
+      projection: { _id: 1, email: 1, role: 1 },
+    };
+    const result = await collection.find({}, options).toArray();
+    resp.json(result);
   },
-  postUser: async(req,resp) =>{
+  postUser: async (req, resp) => {
     try {
       const { email, password, role } = req.body;
       const db = await connect();
-      const listRole = ['admin','waiter','chef'];
-      if(!listRole.includes(role)) resp.status(400).json({ error: 'role is not valid' });
+      const listRole = ["admin", "waiter", "chef"];
+      if (!listRole.includes(role))
+        resp.status(400).json({ error: "role is not valid" });
       const saltRound = 10;
       const salt = await bcrypt.genSalt(saltRound);
       const hashedPassword = await bcrypt.hash(password, salt);
@@ -35,8 +35,8 @@ module.exports = {
       //   resp.send("El usuario ya existe");
       // }
     } catch (error) {
-      console.log("🚀 ~ app.post ~ error:", error)
-      
+      console.log("🚀 ~ app.post ~ error:", error);
+
       return resp.status(500).send("Error en el servidor");
     }
   },
