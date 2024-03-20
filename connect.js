b3893728a84ee34e6ed61cbb3e6d566072f97b6e
Aylin <aylin_santacruz@hotmail.com>
2024-03-20 15:32:00 -0500

    feat(connect.js):Added MongoDB connection setup and connect function

--- connect.js
+++ connect.js
@@ -1,10 +1,21 @@
-const config = require('./config');
+const { MongoClient } = require("mongodb");
+const config = require("./config");
 
 // eslint-disable-next-line no-unused-vars
 const { dbUrl } = config;
+const client = new MongoClient(dbUrl);
+
 
 async function connect() {
   // TODO: Database Connection
+  try{
+    await client.connect();
+    const db = client.db("burgerQueenApi");
+    console.log("conexion correcta")
+    return db;
+  }catch(error){
+    console.log("Error en la conexión", error)
+  }
 }
 
 module.exports = { connect };
