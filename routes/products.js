255c0b07e3fe4526783d2d4ae51a632d7c1df9b1
Aylin <aylin_santacruz@hotmail.com>
2024-03-20 15:41:32 -0500

    feat(product.js):Route configuration to get products with authentication required

--- routes/products.js
+++ routes/products.js
@@ -2,23 +2,19 @@ const {
   requireAuth,
   requireAdmin,
 } = require('../middleware/auth');
+const {getProducts,postProducts,patchProductById,deleteProductById,getProductById} = require('../controller/products.js')
 
 module.exports = (app, nextMain) => {
 
-  app.get('/products', requireAuth, (req, resp, next) => {
-  });
+  app.get('/products', requireAuth, getProducts);
 
-  app.get('/products/:productId', requireAuth, (req, resp, next) => {
-  });
+  app.get('/products/:productId',requireAuth);
 
-  app.post('/products', requireAdmin, (req, resp, next) => {
-  });
+  app.post('/products',requireAdmin)
 
-  app.put('/products/:productId', requireAdmin, (req, resp, next) => {
-  });
+  app.patch('/products/:productId',requireAdmin);
 
-  app.delete('/products/:productId', requireAdmin, (req, resp, next) => {
-  });
+  app.delete('/products/:productId',requireAdmin);
 
   nextMain();
 };
