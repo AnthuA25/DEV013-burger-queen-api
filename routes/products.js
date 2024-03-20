0c41162d010cfbd9a03a3b5c4f0c70834a7c48b6
Aylin <aylin_santacruz@hotmail.com>
2024-03-20 15:43:48 -0500

    feat(product.js):Route configuration to update  products by ID  with authentication required

--- routes/products.js
+++ routes/products.js
@@ -12,7 +12,7 @@ module.exports = (app, nextMain) => {
 
   app.post('/products',requireAdmin,postProducts)
 
-  app.patch('/products/:productId',requireAdmin);
+  app.patch('/products/:productId',requireAdmin,patchProductById);
 
   app.delete('/products/:productId',requireAdmin);
 
