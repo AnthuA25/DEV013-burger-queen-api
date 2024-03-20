07e7ede4026f92a25d94363dd7df5c76485fcec4
Aylin <aylin_santacruz@hotmail.com>
2024-03-20 15:52:44 -0500

    feat(auth.js): Verify user identity

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,7 @@
 const jwt = require('jsonwebtoken');
 
+
+
 module.exports = (secret) => (req, resp, next) => {
   const { authorization } = req.headers;
 
@@ -8,30 +10,41 @@ module.exports = (secret) => (req, resp, next) => {
   }
 
   const [type, token] = authorization.split(' ');
+  console.log("🚀 ~ token:", token)
+  console.log("🚀 ~ type:", type)
+
+  
 
   if (type.toLowerCase() !== 'bearer') {
     return next();
   }
 
   jwt.verify(token, secret, (err, decodedToken) => {
+    console.log("🚀 ~ jwt.verify ~ decodedToken:", decodedToken)
     if (err) {
       return next(403);
     }
-
     // TODO: Verify user identity using `decodeToken.uid`
+    req.userId = decodedToken._id;
+    console.log("🚀 ~ jwt.verify ~ req.userId:", req.userId)
+    req.role = decodedToken.role;
+    console.log("🚀 ~ jwt.verify ~ req.role:", req.role)
+    next()
   });
 };
 
+// Funciones de autenticación y autorización
 module.exports.isAuthenticated = (req) => (
   // TODO: Decide based on the request information whether the user is authenticated
   false
 );
+  
 
 module.exports.isAdmin = (req) => (
+  // if the user is an admin
   // TODO: Decide based on the request information whether the user is an admin
   false
 );
-
 module.exports.requireAuth = (req, resp, next) => (
   (!module.exports.isAuthenticated(req))
     ? next(401)
@@ -46,3 +59,4 @@ module.exports.requireAdmin = (req, resp, next) => (
       ? next(403)
       : next()
 );
+
