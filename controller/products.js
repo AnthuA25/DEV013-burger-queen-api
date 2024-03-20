a23a5c961c7cee9ebd0a92d88117aaa0de21a073
Aylin <aylin_santacruz@hotmail.com>
2024-03-20 15:29:15 -0500

    feat(products.js):Adds patchProductById function to partially update products by ID

--- controller/products.js
+++ controller/products.js
@@ -36,4 +36,25 @@ module.exports = {
       console.log("🚀 ~ getProductById:async ~ error:", error);
     }
   },
+  patchProductById: async (req, resp) => {
+    try {
+      const { productId } = req.params;
+      const { name, price, image, type } = req.body;
+      const db = await connect();
+      const collection = db.collection("products");
+      const filter = { _id: new ObjectId(productId) };
+      const cursor = await collection.updateOne(filter, {
+        $set: {
+          name,
+          price,
+          image,
+          type,
+        },
+      });
+      resp.json(cursor);
+    } catch (error) {
+      console.log("🚀 ~ putProductById: ~ error:", error);
+      resp.json({ msg: "error" });
+    }
+  },
 };
