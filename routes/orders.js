43c9c800109dcc08f48d38ea422c556aeca2f762
Aylin <aylin_santacruz@hotmail.com>
2024-04-01 16:05:21 -0500

    feat(orders.js): Order routing management with authentication

--- routes/orders.js
+++ routes/orders.js
@@ -1,22 +1,17 @@
 const {
   requireAuth,
 } = require('../middleware/auth');
-
+const { postOrders, getOrders, getOrderById, putOrderById, deleteOrderById} = require('../controller/orders')
 module.exports = (app, nextMain) => {
-  app.get('/orders', requireAuth, (req, resp, next) => {
-  });
+  app.get('/orders', requireAuth, getOrders);
 
-  app.get('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  app.get('/orders/:orderId', requireAuth, getOrderById);
 
-  app.post('/orders', requireAuth, (req, resp, next) => {
-  });
+  app.post('/orders', requireAuth, postOrders);
 
-  app.put('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  app.put('/orders/:orderId', requireAuth, putOrderById);
 
-  app.delete('/orders/:orderId', requireAuth, (req, resp, next) => {
-  });
+  app.delete('/orders/:orderId', deleteOrderById);
 
   nextMain();
 };
